8f995a504d35a4cd54e3a885a447ab0ab6313722
nikkakiffar <nikeim18@gmail.com>
2022-06-20 10:29:53 +0300

    fix migrations

--- Token/migrations/1_initial_migration.js
+++ Token/migrations/1_initial_migration.js
@@ -1,14 +1,14 @@
 const Migrations = artifacts.require("Migrations");
 const Memorial = artifacts.require("MultisigMemorial")
 const Pain = artifacts.require("MultisigPain")
-const MockDAO = artifacts.require("MockDAO")
+const DAO = artifacts.require("DAO")
 
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(Migrations);
   const deployedMemorial = await deployer.deploy(Memorial, "Memorial", "MEM", 30000, accounts.slice(0, 3))
   const deployedPain = await deployer.deploy(Pain, "Pain", "PAIN", 30000, accounts.slice(0, 3))
-  const deployedMockDAO = await deployer.deploy(MockDAO, deployedMemorial.address, deployedPain.address)
-  // console.log("deployedMemorial: ", deployedMemorial.address)
-  // console.log("deployedPain: ", deployedPain.address)
-  // console.log("deployedMockDAO: ", deployedMockDAO.address)
+  const deployedDAO = await deployer.deploy(DAO, deployedMemorial.address, deployedPain.address)
+  console.log("deployedMemorial: ", deployedMemorial.address)
+  console.log("deployedPain: ", deployedPain.address)
+  console.log("deployedDAO: ", deployedDAO.address)
 };
